b7cecdb3674503c29131a504326ca0de0ddc21e7
Martheangelique <angeliquemarthe27@gmail.com>
2026-04-28 19:36:56 +0100

    Delete marthy_app directory

--- marthy_app/core/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# On indique à Django où se trouve le fichier settings.py
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-application = get_wsgi_application()
